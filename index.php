501c8e4c182546a35a66e54034b5e1b7fc7bbed0
student <student@academyx.com>
2013-04-17 21:02:42 -0700

    my initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- index.php -->
+<?php
+$db = new mysqli ("localhost","root", "passwordx","dollarstore");
+$rs = $db->query("select * from products");
+?>
+<html>
+<head>
+  <title>Product Listing</title>
+</head>
+<body>
+<p><a href="add.php">Add Product</a></p>
+
+<table border="1">
+<tr>
+	<th>id</th>
+	<th>Product Name</th>
+	<th>Price</th>
+	<th>Action</th>
+</tr>
+
+<?php while ($r = $rs->fetch_assoc()) { ?>
+<tr>
+	<td><?php echo $r["id"]; ?></td>
+	<td><?php echo $r["name"]; ?></td>
+	<td><?php echo $r["priceS"]; ?></td>
+    <td><a href="edit.php?id=<?php echo $r["id"]; ?>">Edit</a>
+    <a href="delete.php?id=<?php echo $r["id"]; ?>">Delete</a></td>
+</tr>
+<?php } ?>
+</table>
+</body>
+</html>
